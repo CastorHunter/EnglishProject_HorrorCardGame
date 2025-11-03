cf86c54d991fc521dce6994f30998fde8638bdeb
ESMA-JulienP <148343029+CastorHunter@users.noreply.github.com>
2025-11-03 12:30:41 +0100

    [ADD] Enums

---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public enum DamageType
+{
+    Metal,
+    Wood,
+    Fire,
+    Water,
+    Heal,
+    Cold,
+    Electric,
+    Poison,
+    Light
+}
+
+public enum State
+{
+    Wisdom,
+    Feared,
+    AbyssalEnd,
+    Surprised,
+    Empathetic
+}
